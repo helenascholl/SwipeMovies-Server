2006a0ffc095b00cf11c34ab453e49a7318d08ed
schollsebastian <scholl.sebastian@gmx.at>
2021-03-23 20:11:44 +0100

    Implement /movies/user/:userId endpoint

--- src/movies.ts
+++ src/movies.ts
@@ -1,7 +1,27 @@
 import express from 'express';
+import HttpStatus from 'http-status-codes';
+import { SwipedMovie, isSwipedMovie } from './movie';
+
+const swipedMovies: Map<string, Map<number, SwipedMovie>> = new Map<string, Map<number, SwipedMovie>>();
 
 const movies = express();
 
-movies.get('/', ((req, res) => res.send('movies')));
+movies.post('/user/:userId', (req, res) => {
+  const userId = req.params['userId'];
+
+  if (isSwipedMovie(req.body)) {
+    const swipedMovie: SwipedMovie = req.body;
+
+    if (!swipedMovies.get(userId)) {
+      swipedMovies.set(userId, new Map<number, SwipedMovie>());
+    }
+
+    swipedMovies.get(userId)!.set(swipedMovie.movie.id, swipedMovie);
+    res.status(HttpStatus.OK)
+      .send(JSON.stringify(swipedMovie));
+  } else {
+    res.sendStatus(HttpStatus.BAD_REQUEST);
+  }
+});
 
 export default movies;
