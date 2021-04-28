aac9374f293730d974addfcc8dba98b4dea11287
schollsebastian <scholl.sebastian@gmx.at>
2021-04-28 21:05:13 +0200

    Create POST /api/groups endpoint

--- src/endpoints/groups.ts
+++ src/endpoints/groups.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import GroupRepository from '../repositories/groupRepository';
 import HttpStatus from 'http-status-codes';
+import Group from '../group';
 
 const groupRepository = GroupRepository.getInstance();
 
@@ -9,6 +10,18 @@ const groups = express.Router();
 groups.get('/', (_, res) => {
   res.status(HttpStatus.OK)
     .send(groupRepository.getAll());
+});
+
+groups.post('/', (req, res) => {
+  try {
+    const group = Group.parse(req.body);
+    groupRepository.add(group);
+
+    res.status(HttpStatus.OK)
+      .send(group);
+  } catch (e) {
+    res.sendStatus(HttpStatus.BAD_REQUEST);
+  }
 });
 
 export default groups;
