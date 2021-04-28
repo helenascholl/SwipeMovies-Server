1372acda8e1c72778558229a59ec6ae6e66d853d
schollsebastian <scholl.sebastian@gmx.at>
2021-04-29 00:15:51 +0200

    Create POST /api/users/:userId/groups endpoint

--- src/endpoints/users/groups.ts
+++ src/endpoints/users/groups.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import UserRepository from '../../repositories/userRepository';
 import HttpStatus from 'http-status-codes';
+import UserRepository from '../../repositories/userRepository';
 import UserGroupRepository from '../../repositories/userGroupRepository';
+import GroupRepository from '../../repositories/groupRepository';
 
 const userRepository = UserRepository.getInstance();
+const groupRepository = GroupRepository.getInstance();
 const userGroupRepository = UserGroupRepository.getInstance();
 
 const groups = express.Router();
@@ -14,6 +16,19 @@ groups.get('/:userId/groups', (req, res) => {
   if (user) {
     res.status(HttpStatus.OK)
       .send(userGroupRepository.findGroupsByUserId(user.id));
+  } else {
+    res.sendStatus(HttpStatus.NOT_FOUND);
+  }
+});
+
+groups.post('/:userId/groups', (req, res) => {
+  const user = userRepository.get(parseInt(req.params['userId']));
+  const group = groupRepository.get(parseInt(req.body['id']));
+
+  if (user && group) {
+    userGroupRepository.add(user, group);
+    res.status(HttpStatus.OK)
+      .send(group);
   } else {
     res.sendStatus(HttpStatus.NOT_FOUND);
   }
