8d478374b90c45d4b0373eb42b7eb1c41a07b9dc
schollsebastian <scholl.sebastian@gmx.at>
2021-04-28 17:51:46 +0200

    Implement SwipedMovieRepository.add

--- src/repositories/swipedMovieRepository.ts
+++ src/repositories/swipedMovieRepository.ts
@@ -1,7 +1,14 @@
+import { SwipedMovie } from '../movie';
+
 export default class SwipedMovieRepository {
   private static instance: SwipedMovieRepository;
+  private swipedMovies: Map<number, SwipedMovie>;
+  private currentId: number;
 
-  private constructor() {}
+  private constructor() {
+    this.swipedMovies = new Map<number, SwipedMovie>();
+    this.currentId = 0;
+  }
 
   public static getInstance(): SwipedMovieRepository {
     if (!this.instance) {
@@ -9,5 +16,12 @@ export default class SwipedMovieRepository {
     }
 
     return this.instance;
+  }
+
+  public add(swipedMovie: SwipedMovie): SwipedMovie {
+    swipedMovie.movie.id = this.currentId;
+    this.swipedMovies.set(this.currentId++, swipedMovie);
+
+    return swipedMovie;
   }
 }
