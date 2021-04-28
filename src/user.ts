5cf879c0499a4786ed7f7caaccdd3d4177687fd7
schollsebastian <scholl.sebastian@gmx.at>
2021-04-28 16:18:47 +0200

    Implement User.parse

--- src/user.ts
+++ src/user.ts
@@ -11,6 +11,18 @@ export default class User {
     this.swipedMovies = new Map<number, SwipedMovie>();
   }
 
+  public static parse(obj: any): User {
+    if (
+      obj &&
+      (typeof obj.id === 'number' || typeof obj.id === 'undefined') &&
+      typeof obj.username === 'string'
+    ) {
+      return new User(obj.id, obj.username);
+    } else {
+      throw Error('Cannot parse object to User');
+    }
+  }
+
   public addMovie(swipedMovie: SwipedMovie) {
     this.swipedMovies.set(swipedMovie.movie.id, swipedMovie);
   }
