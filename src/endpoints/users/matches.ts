db7a7f0169f923e01fd5edd252d663f2b97e8b01
schollsebastian <scholl.sebastian@gmx.at>
2021-05-18 19:30:02 +0200

    Move finding matches to getMatches()

--- src/endpoints/users/matches.ts
+++ src/endpoints/users/matches.ts
@@ -15,54 +15,58 @@ const swipedMovieRepository = SwipedMovieRepository.getInstance();
 matches.get('/:userId/matches', async (req, res) => {
   if (req.params['userId']) {
     const user = userRepository.get(parseInt(req.params['userId']));
-    const matches: Match[] = [];
 
     if (user) {
-      const groups = userGroupRepository.findGroupsByUserId(user.id);
-      const swipedMovies = swipedMovieRepository.findByUserId(user.id)
-      const users: User[] = [];
-
-      for (const group of groups) {
-        for (const groupUser of userGroupRepository.findUsersByGroupId(group.id)) {
-          if (groupUser.id !== user.id) {
-            let userExists = false;
-
-            for (const userInArray of users) {
-              if (groupUser.id === userInArray.id) {
-                userExists = true;
-              }
-            }
-
-            if (!userExists) {
-              users.push(groupUser);
-            }
-          }
-        }
-      }
-
-      for (const groupUser of users) {
-        for (const groupUserSwipedMovie of swipedMovieRepository.findByUserId(groupUser.id)) {
-          for (const swipedMovie of swipedMovies) {
-            if (
-              groupUserSwipedMovie.movie.id === swipedMovie.movie.id
-              && groupUserSwipedMovie.swipeDirection === SwipeDirection.RIGHT
-              && swipedMovie.swipeDirection === SwipeDirection.RIGHT
-            ) {
-              matches.push({
-                movie: swipedMovie.movie,
-                user: groupUser
-              });
-            }
-          }
-        }
-      }
-
       res.status(HttpStatus.OK)
-        .send(matches);
+        .send(getMatches(user));
     } else {
       res.sendStatus(HttpStatus.NOT_FOUND);
     }
   }
 });
+
+function getMatches(user: User): Match[] {
+  const matches: Match[] = [];
+  const users: User[] = [];
+  const groups = userGroupRepository.findGroupsByUserId(user.id);
+  const swipedMovies = swipedMovieRepository.findByUserId(user.id);
+
+  for (const group of groups) {
+    for (const groupUser of userGroupRepository.findUsersByGroupId(group.id)) {
+      if (groupUser.id !== user.id) {
+        let userExists = false;
+
+        for (const userInArray of users) {
+          if (groupUser.id === userInArray.id) {
+            userExists = true;
+          }
+        }
+
+        if (!userExists) {
+          users.push(groupUser);
+        }
+      }
+    }
+  }
+
+  for (const groupUser of users) {
+    for (const groupUserSwipedMovie of swipedMovieRepository.findByUserId(groupUser.id)) {
+      for (const swipedMovie of swipedMovies) {
+        if (
+          groupUserSwipedMovie.movie.id === swipedMovie.movie.id
+          && groupUserSwipedMovie.swipeDirection === SwipeDirection.RIGHT
+          && swipedMovie.swipeDirection === SwipeDirection.RIGHT
+        ) {
+          matches.push({
+            movie: swipedMovie.movie,
+            user: groupUser
+          });
+        }
+      }
+    }
+  }
+
+  return matches;
+}
 
 export default matches;
