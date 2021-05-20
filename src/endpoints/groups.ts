27405958f8d95c76ceb9fb86d63d689117d1c0e7
schollsebastian <scholl.sebastian@gmx.at>
2021-05-20 09:40:45 +0200

    Remove joining group after creating it

--- src/endpoints/groups.ts
+++ src/endpoints/groups.ts
@@ -1,13 +1,9 @@
 import express from 'express';
 import HttpStatus from 'http-status-codes';
 import GroupRepository from '../repositories/groupRepository';
-import UserRepository from '../repositories/userRepository';
-import UserGroupRepository from '../repositories/userGroupRepository';
 import Group from '../group';
 
-const userRepository = UserRepository.getInstance();
 const groupRepository = GroupRepository.getInstance();
-const userGroupRepository = UserGroupRepository.getInstance();
 
 const groups = express.Router();
 
@@ -17,25 +13,13 @@ groups.get('/', (_, res) => {
 });
 
 groups.post('/', (req, res) => {
-  if (req.body['user'] !== undefined && req.body.user['id'] !== undefined) {
-    const user = userRepository.get(parseInt(req.body.user['id']));
+  try {
+    const group = Group.parse(req.body);
+    groupRepository.add(group);
 
-    if (user) {
-      try {
-        const group = Group.parse(req.body.group);
-
-        groupRepository.add(group);
-        userGroupRepository.add(user, group);
-
-        res.status(HttpStatus.OK)
-          .send(group);
-      } catch (e) {
-        res.sendStatus(HttpStatus.BAD_REQUEST);
-      }
-    } else {
-      res.sendStatus(HttpStatus.NOT_FOUND);
-    }
-  } else {
+    res.status(HttpStatus.OK)
+      .send(group);
+  } catch (e) {
     res.sendStatus(HttpStatus.BAD_REQUEST);
   }
 });
