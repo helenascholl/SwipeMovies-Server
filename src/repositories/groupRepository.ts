3f23c45fa5b9f51ba1656e0678ba6c3543214951
schollsebastian <scholl.sebastian@gmx.at>
2021-06-16 13:24:41 +0200

    Persist groups in groups.json

--- src/repositories/groupRepository.ts
+++ src/repositories/groupRepository.ts
@@ -1,13 +1,15 @@
 import Group from '../group';
+import RepositoryBase from './repositoryBase';
 
-export default class GroupRepository {
+export default class GroupRepository extends RepositoryBase<Group> {
   private static instance: GroupRepository;
   private groups: Map<number, Group>;
   private currentId: number;
 
   private constructor() {
-    this.groups = new Map<number, Group>();
-    this.currentId = 0;
+    super();
+    this.groups = this.parseJsonFile('src/data/groups.json');
+    this.currentId = this.groups.size;
   }
 
   public static getInstance(): GroupRepository {
@@ -25,6 +27,8 @@ export default class GroupRepository {
   public add(group: Group): Group {
     group.id = this.currentId;
     this.groups.set(this.currentId++, group);
+
+    this.writeToFile(Array.from(this.groups.values()), 'src/data/groups.json');
 
     return group;
   }
